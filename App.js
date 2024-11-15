f2c49d18ee29ad2d48e1cd19fa0a0f471daeb391
Melissa Yefsah <melissa.yefsah@melissa.yefsah-MacBook-Pro>
2024-11-15 11:32:09 -0500

    Added Weather card

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 // Import data and WeatherCard here
+import cities from './data.js';
+import WeatherCard from './components/WeatherCard.js'
 
 
 function App() {
@@ -8,10 +10,7 @@ function App() {
             <h1 className = "title">REACTIVE WEATHER</h1>
             <h3 className = "subtitle">Up to the minute weather news</h3>
             <div className = "app">
-                {/* Render components here */}
-                
-
-
+                { cities.map((weather) => <WeatherCard data= {weather} />) }
             </div>
             
         </>
